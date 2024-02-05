cbefaca3f7ed60d841414673c2f2c36b05bc912b
Samuel Sales <78183920+Samuel-Sales@users.noreply.github.com>
2024-02-05 09:54:32 -0300

    Update env.jsx

--- src/config/env.jsx
+++ src/config/env.jsx
@@ -1,5 +1,5 @@
 export const env = {
     production: {
-        theMovieDBToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MDIwMjQ3YzczZmI5YmE0YWFhZTBjOTE3MTgyYjU2NiIsInN1YiI6IjY0ZGQ3YTc2NWFiODFhMDExYzJkOWIyYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6-N9iD424rnXaWua7e--DJLL_goVxu9qt3JjHgmelxg'
+        theMovieDBToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NDc0NTc5YmU4MWMzZTk1MTQ5ZWEzZjRjZTkwMWRjMCIsInN1YiI6IjY0ZGQ3YTc2NWFiODFhMDExYzJkOWIyYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SipTBo5GyNvXdI-fp2_ojzeXnfjq90o6JCwRSVlhyv4'
     }
 }
